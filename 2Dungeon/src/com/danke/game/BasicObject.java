3de56798039e4128d931fe06f82eefb91a0c9560
LazyTurtleknight <tube3719@gmail.com>
2020-11-18 16:44:18 +0100

    added a basic class for in game objects

---
+++ 2Dungeon/src/com/danke/game/BasicObject.java
@@ -0,0 +1,48 @@
+package com.danke.game;
+
+import com.danke.engine.gfx.Sprite;
+
+public class BasicObject {
+	private int offsetX;
+	private int offsetY;
+	private Sprite sprite;
+	private Boolean indistructable = true;
+	private Boolean immovable = true;
+	
+	public BasicObject(int offsetX, int offsetY, Sprite sprite) {
+		
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.sprite = sprite;
+		
+	}
+
+	//getter and setter
+	public Boolean getIndistructable() {
+		return indistructable;
+	}
+
+	public void setIndistructable(Boolean indistructable) {
+		this.indistructable = indistructable;
+	}
+
+	public Boolean getImmovable() {
+		return immovable;
+	}
+
+	public void setImmovable(Boolean immovable) {
+		this.immovable = immovable;
+	}
+
+	public int getOffsetX() {
+		return offsetX;
+	}
+
+	public int getOffsetY() {
+		return offsetY;
+	}
+
+	public Sprite getSprite() {
+		return sprite;
+	}
+}
