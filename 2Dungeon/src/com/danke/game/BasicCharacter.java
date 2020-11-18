cd41210b4672cb136d710eb8eb8858964623920d
LazyTurtleknight <tube3719@gmail.com>
2020-11-18 16:47:59 +0100

    implemented basic movement

--- 2Dungeon/src/com/danke/game/BasicCharacter.java
+++ 2Dungeon/src/com/danke/game/BasicCharacter.java
@@ -2,22 +2,63 @@ package com.danke.game;
 
 import com.danke.engine.gfx.Sprite;
 
-public class Character {
+public class BasicCharacter {
 
 	private int offsetX;
 	private int offsetY;
 	private Sprite sprite;
-	
-	public Character(String path, int offsetX, int offsetY) {
+	private String name;	
+
+	public BasicCharacter(Sprite sprite, int offsetX, int offsetY) {
 		
 		this.offsetX = offsetX;
 		this.offsetY = offsetY;
-		
-		sprite = new Sprite(path);
+		this.sprite = sprite;
 		
 	}
 	
-	public void move() {
-		
+	public void moveUp() {
+		offsetY--;
+	}
+
+	public void moveDown() {
+		offsetY++;
+	}
+
+	public void moveRight() {
+		offsetX++;
+	}
+	
+	public void moveLeft() {
+		offsetX--;
+	}
+
+	//getter and setter
+	public int getOffsetX() {
+		return offsetX;
+	}
+
+	public void setOffsetX(int offsetX) {
+		this.offsetX = offsetX;
+	}
+
+	public int getOffsetY() {
+		return offsetY;
+	}
+
+	public void setOffsetY(int offsetY) {
+		this.offsetY = offsetY;
+	}
+
+	public Sprite getSprite() {
+		return sprite;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
