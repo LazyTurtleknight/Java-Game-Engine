75a63d02017a5ee48a1a9ce484cc5b53d2d4664c
LazyTurtleknight <tube3719@gmail.com>
2021-09-15 14:12:07 +0200

    Add error check if the width and height of a sprite sheet divided by the given width and height of an individual sprite is even

--- 2Dungeon/src/com/engine/gfx/SpriteAnimation.java
+++ 2Dungeon/src/com/engine/gfx/SpriteAnimation.java
@@ -1,16 +1,26 @@
 package com.engine.gfx;
 
+/*
+ * Class to handle animations. 
+ */
+
 public class SpriteAnimation extends SpriteSheet {
 	
-	//may not be needed ?
 	private int numberOfFrames;
 
-	//loads an image of multiple sprite that form the animation
+	//loads an image of multiple sprites that form an animation
 	public SpriteAnimation(String path, int spriteWidth, int spriteHeight) {
 		super(path, spriteWidth, spriteHeight);
 
-		//TODO: check if this is even division
-		this.numberOfFrames = this.getHeight()/spriteHeight * this.getWidth()/spriteWidth;
+		//check if the given width and height divide the width and height of the image
+		if(this.getHeight() % spriteHeight == 0 & this.getWidth() % spriteWidth == 0) {
+			
+			this.numberOfFrames = this.getHeight()/spriteHeight * this.getWidth()/spriteWidth;
+		}else {
+			
+			//TODO: implement appropriate error handling
+		}
+		
 		
 	}
 	
